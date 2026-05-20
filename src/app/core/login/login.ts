737d46268aea92cd061be073dbb836be2d9ee7e1
shaileshljadhav <shaileshjadhav403@gmail.com>
2026-05-20 23:37:09 +0530

    Primeng installation and setup and login page ui

---
+++ src/app/core/login/login.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
+@Component({ 
+  selector: 'app-login',
+  imports: [ReactiveFormsModule,CardModule, InputTextModule, PasswordModule, ButtonModule, FloatLabelModule],
+  templateUrl: './login.html',
+  styleUrl: './login.scss',
+})
+export class Login implements OnInit{
+
+  constructor(){  }
+
+  loginForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [
+        Validators.required, 
+        Validators.email
+      ]),
+      password: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(6)
+      ])
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      console.log('Login Data:', this.loginForm.value);
+      // Implement your authentication API service call here
+    }
+  }
+}
