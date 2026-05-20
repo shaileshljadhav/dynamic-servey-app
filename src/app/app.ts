1cb462997aa32e426b512362e111dadb8e9a949e
shaileshljadhav <shaileshjadhav403@gmail.com>
2026-05-21 00:10:58 +0530

    Menus added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,36 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('dynamic-servey-app');
+  constructor(private router: Router) {
+    console.log(this.router.url);
+  }
+
+  items: MenuItem[] | undefined;
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Dashboard',
+        icon: 'pi pi-home'
+      },
+      {
+        label: 'Servey Builder',
+        icon: 'pi pi-star'
+      },
+      // {
+      //     label: 'Contact',
+      //     icon: 'pi pi-envelope'
+      // }
+    ];
+  }
 }
