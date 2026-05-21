3c3b4f36550eae022b692c9c0ced939bffaed2ce
shaileshljadhav <shaileshjadhav403@gmail.com>
2026-05-21 08:51:26 +0530

    Chart js installed and sample chart setup under dashbord

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,9 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartModule } from 'primeng/chart';
+import { Chart, registerables} from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ChartModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  basicData: any;
+  basicOptions: any;
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    // if (isPlatformBrowser(this.platformId)) {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--p-text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+    const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+    this.basicData = {
+      labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+      datasets: [
+        {
+          label: 'Sales',
+          data: [540, 325, 702, 620],
+          backgroundColor: ['rgba(249, 115, 22, 0.2)', 'rgba(6, 182, 212, 0.2)', 'rgb(107, 114, 128, 0.2)', 'rgba(139, 92, 246, 0.2)'],
+          borderColor: ['rgb(249, 115, 22)', 'rgb(6, 182, 212)', 'rgb(107, 114, 128)', 'rgb(139, 92, 246)'],
+          borderWidth: 1
+        }
+      ]
+    };
+
+    this.basicOptions = {
+      maintainAspectRatio: false,
+      aspectRatio: 0.8,
+      plugins: {
+        legend: {
+          labels: {
+            color: textColor
+          }
+        }
+      },
+      scales: {
+        x: {
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        },
+        y: {
+          beginAtZero: true,
+          ticks: {
+            color: textColorSecondary
+          },
+          grid: {
+            color: surfaceBorder
+          }
+        }
+      }
+    };
+    // }
+  }
+}
