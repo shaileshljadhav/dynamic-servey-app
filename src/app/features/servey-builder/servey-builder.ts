8bbbdfe0ce522f028b0aeb91545a9a4975c1f87b
shaileshljadhav <shaileshjadhav403@gmail.com>
2026-05-21 10:42:39 +0530

    Servey builder form ui started

--- src/app/features/servey-builder/servey-builder.ts
+++ src/app/features/servey-builder/servey-builder.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-servey-builder',
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    SelectModule,
+    ButtonModule
+  ],
   templateUrl: './servey-builder.html',
   styleUrl: './servey-builder.scss',
 })
-export class ServeyBuilder {}
+export class ServeyBuilder implements OnInit {
+  serveyForm!: FormGroup;
+  questionTypes: any[] = [];
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.questionTypes = [
+      { name: 'Text', code: '1' },
+      { name: 'Multiple Choice ', code: '2' },
+      { name: 'Checkbox ', code: '3' }
+    ];
+
+    this.serveyForm = this.fb.group({
+      question: ['', [Validators.required, Validators.minLength(2)]],
+      questionType: [null, Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.serveyForm.valid) {
+      console.log('Form Submitted:', this.serveyForm.value);
+    }
+  }
+}
